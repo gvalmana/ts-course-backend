0516ac14b2552dbd0b66f53fb33201dc3864ba08
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2025-02-19 22:48:24 -0500

    second commit

--- src/Server.ts
+++ src/Server.ts
@@ -1,5 +1,6 @@
 import express, {NextFunction, Request, Response} from 'express'
 import employeesRouter from './api/empl/Employee.route';
+import reportsRouter from './api/reports/Reports.route';
 
 const port = 3000;
 
@@ -10,6 +11,8 @@ export class Server {
     startServer(){
 
         this.app.use('/employees', employeesRouter)
+        this.app.use('/reports', reportsRouter)
+
         this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
             console.error(err.stack)
             res.send(err.message)
