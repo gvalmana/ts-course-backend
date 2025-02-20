893152c877e13fd7d69c31aabe13c242692faa2a
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2025-02-19 22:40:16 -0500

    eliminiado cache

--- src/api/empl/Employee.model.ts
+++
@@ -1,9 +0,0 @@
-type Position = 'Manager' | 'HR' | 'Engineer'
-
-export type Employee = {
-    id: string;
-    name: string;
-    employedAt: Date;
-    position: Position;
-    salary: number;
-}
--- src/data/IdGenerator.ts
+++
@@ -1,6 +0,0 @@
-import { randomBytes } from 'crypto'
-
-export function generateRandomId() {
-    const randomId = randomBytes(10).toString('hex');
-    return randomId;
-}
