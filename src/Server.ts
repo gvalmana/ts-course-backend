20971350e225d9ebffb661f31a4e7e1ad972d610
Gustavo Valmaña Villalonga <gustavo.valmana@alegra.com>
2025-02-19 22:18:12 -0500

    first commit

---
+++ src/Server.ts
@@ -0,0 +1,20 @@
+import express, {Request, Response} from 'express'
+import employeesRouter from './api/empl/Employee.route';
+
+const port = 3000;
+
+export class Server {
+
+    private app = express();
+
+    startServer(){
+
+        this.app.use('/employees', employeesRouter)
+        this.app.listen(port, ()=>{
+            console.log('Listening on port ' + port)
+        })
+    }
+
+}
+
+new Server().startServer()
